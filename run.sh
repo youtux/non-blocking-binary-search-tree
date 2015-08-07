2f743071beb238155f1c0367b761057fab893053
Alessio Bogon <youtux@gmail.com>
2015-08-07 16:52:18 +0200

    run.sh: move declarations

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-CLASSPATH="$(pwd)/src"
-
-MAIN_CLASS="it.unitn.studenti.alessiobogon.concurrency.Main"
-MAIN_FILE_PATH="$CLASSPATH/it/unitn/studenti/alessiobogon/concurrency/Main.java"
-
 # Log levels:
 # INFO: Show only infos and errors
 # FINE: Show entering and exiting parameters for main
@@ -14,8 +9,12 @@ MAIN_FILE_PATH="$CLASSPATH/it/unitn/studenti/alessiobogon/concurrency/Main.java"
 # FINEST: Trace also the various CAS operations
 LOGLEVEL="FINE"
 
+CLASSPATH="$(pwd)/src"
+MAIN_CLASS_NAME="it.unitn.studenti.alessiobogon.concurrency.Main"
+MAIN_FILE_PATH="$CLASSPATH/it/unitn/studenti/alessiobogon/concurrency/Main.java"
+
 echo "Compiling classes..."
 javac -cp "$CLASSPATH" "$MAIN_FILE_PATH"
 
-echo "Running $MAIN_CLASS"
-java -DlogLevel=$LOGLEVEL -cp "$CLASSPATH" "$MAIN_CLASS"
+echo "Running $MAIN_CLASS_NAME"
+java -DlogLevel=$LOGLEVEL -cp "$CLASSPATH" "$MAIN_CLASS_NAME"
