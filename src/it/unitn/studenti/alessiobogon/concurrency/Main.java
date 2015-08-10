2a74f336cc42e753ac7fedc99ac7ffe30e1afbb2
Alessio Bogon <youtux@gmail.com>
2015-08-10 18:06:05 +0200

    Main: 5 threads

--- src/it/unitn/studenti/alessiobogon/concurrency/Main.java
+++ src/it/unitn/studenti/alessiobogon/concurrency/Main.java
@@ -62,45 +62,61 @@ public class Main {
 
         Thread t1 = new Thread(){
             public void run(){
-                bst.insert(1);
-                bst.insert(0);
-                bst.delete(3);
-                bst.insert(7);
-                bst.insert(8);
-                bst.find(1);
+                bst.insert(20);
+                bst.find(0);
+                bst.insert(10);
+                bst.delete(30);
+                bst.insert(-20);
             }
         };
         Thread t2 = new Thread(){
             public void run(){
-                bst.insert(2);
-                bst.find(1);
-                bst.delete(2);
-                bst.delete(5);
-                bst.insert(9);
-                bst.delete(4);
-                bst.insert(5);
-                bst.find(3);
+                bst.insert(0);
+                bst.delete(0);
+                bst.find(20);
+                bst.insert(30);
+                bst.insert(-10);
             }
         };
         Thread t3 = new Thread(){
             public void run(){
-                bst.insert(3);
-                bst.delete(9);
-                bst.insert(1);
-                bst.find(1);
-                bst.insert(6);
-                bst.delete(1);
+                bst.delete(20);
+                bst.insert(30);
+                bst.insert(20);
+                bst.insert(0);
+                bst.find(30);
             }
         };
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        t3.join();
+        Thread t4 = new Thread(){
+            public void run(){
+                bst.insert(30);
+                bst.find(20);
+                bst.insert(15);
+                bst.find(20);
+                bst.insert(5);
+            }
+        };
+
+        Thread t5 = new Thread(){
+            public void run(){
+                bst.insert(25);
+                bst.insert(-5);
+                bst.insert(-15);
+                bst.delete(20);
+                bst.delete(5);
+            }
+        };
+
+        Thread[] threads = {t1, t2, t3, t4, t5};
+
+        for (Thread t : threads) {
+            t.start();
+        }
+        for (Thread t: threads) {
+            t.join();
+        }
 
         System.out.println("The resulting tree is: " + bst);
-
 
         try {
             PrintWriter graphOutput = new PrintWriter(GRAPH_OUTPUT_PATH);
